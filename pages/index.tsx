ab33b93765e5f6dd0f316bfe88abaefdcdcc76b0
Adam Lay <adam93.lay+git@gmail.com>
2022-07-01 15:36:00 +0100

    MUI, AppBar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import { useSelector } from "react-redux";
 import { RootState } from "../data/store";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   const [data, setData] = useState();
@@ -22,9 +23,18 @@ const Home: NextPage = () => {
   }, [authenticated]);
 
   return (
-    <div className="container">
-      <main>Home page!</main>
-    </div>
+    <>
+      <AppBar position="static" elevation={0}>
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} className="title-font">
+            MMF Browser
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="container">
+        <main>Home page!</main>
+      </div>
+    </>
   );
 };
 
